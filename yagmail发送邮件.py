56bf137d79ccc948bcf956164de9d349e55f2d84
zhangzongfen <48074381+zhangzongfen@users.noreply.github.com>
2020-11-07 17:33:32 +0800

    Update yagmail发送邮件.py

--- yagmail发送邮件.py
+++ yagmail发送邮件.py
@@ -1,10 +1,10 @@
 import yagmail
 
-yagindex = yagmail.SMTP(user="1332029758@qq.com", password="aiafuuuiregigijd", host="smtp.qq.com")
+yagindex = yagmail.SMTP(user="2433@qq.com", password="21443", host="smtp.qq.com")
 
 Yag_contents = ['邮件正文']
-# yagindex.send("zhang_zong_fen@sina.com", "yagmail主题", Yag_contents)
+# yagindex.send("23432@sina.com", "yagmail主题", Yag_contents)
 # 多个接收者
-# yagindex.send(["zhang_zong_fen@sina.com", "123@qq.com"], "yagmail主题", Yag_contents)
+# yagindex.send(["23432@sina.com", "123@qq.com"], "yagmail主题", Yag_contents)
 
-yagindex.send("zhang_zong_fen@sina.com", "yagmail附件主题", Yag_contents, ["D:\PyCharm 2019.1.1\workspace\python自动化测试学习\JQuery定位.py"])
+yagindex.send("23432@sina.com", "yagmail附件主题", Yag_contents, ["D:\PyCharm 2019.1.1\workspace\python自动化测试学习\JQuery定位.py"])
